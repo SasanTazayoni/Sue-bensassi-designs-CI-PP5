1d65f3326ba99a119ff8db0ef8e38749bc2b615c
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-04-26 14:28:48 +0000

    Secure secret key

--- sue_bensassi_designs/settings.py
+++ sue_bensassi_designs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rs454ycq52=p+z-t37dy6^2a_c=#lm8wr2e(ds2*6+8vbox^d_'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
