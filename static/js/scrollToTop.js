5e8ad1b9afb514f0c4ebc0966f20d9421312efb6
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-05-09 18:52:21 +0000

    Add scroll to top button with animation

---
+++ static/js/scrollToTop.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector('[data-navbar]');
+const scrollToTop = document.querySelector('[data-scroll-up]');
+
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.scrollY;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > (navHeight * 2)) {
+        scrollToTop.classList.add('show-link');
+    } else {
+        scrollToTop.classList.remove('show-link');
+    }
+});
