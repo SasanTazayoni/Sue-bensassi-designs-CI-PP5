a0b08c2e269ae36a724d29cdc307e6eb00a8962d
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-05-10 18:35:29 +0000

    Added products app and created models for product and Category

---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=200)
+    display_name = models.CharField(max_length=200, default='')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    stock = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.name
