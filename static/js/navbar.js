d01b95261663101d29b88d58c4531d5e26a3252b
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-05-10 12:28:00 +0000

    Add script to detect navbar height for margin adjustment of hero

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -8,3 +8,23 @@ function scrollFunction() {
         navbar.classList.remove("scrolled");
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Function to update margin-top of hero container
+    function updateMarginTop() {
+        // Get the height of the navbar
+        const navbarHeight = document.querySelector('header').offsetHeight + 5; // Add 5 pixels
+
+        // Set the margin-top of the hero container
+        const heroContainer = document.querySelector('.hero-container');
+        if (heroContainer) {
+            heroContainer.style.marginTop = navbarHeight + 'px';
+        }
+    }
+
+    // Initial update
+    updateMarginTop();
+
+    // Update when window is resized
+    window.addEventListener('resize', updateMarginTop);
+});
