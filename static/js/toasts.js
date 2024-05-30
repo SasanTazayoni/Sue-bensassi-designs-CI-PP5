c3fe7301e78fb07056ad3dfc5bb0f58c617ba1c4
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-05-30 18:10:45 +0000

    Update script for toasts to close when clicking off of them

--- static/js/toasts.js
+++ static/js/toasts.js
@@ -1,7 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     const toastElements = document.querySelectorAll('.toast');
+    
     toastElements.forEach(function(toastElement) {
         const toast = new bootstrap.Toast(toastElement);
         toast.show();
+
+        document.addEventListener('click', e => {
+            if (!toastElement.contains(e.target)) {
+                toast.hide();
+            }
+        });
+
+        toastElement.addEventListener('click', e => {
+            e.stopPropagation();
+        });
+
+        const closeButton = toastElement.querySelector('.btn-close');
+
+        if (closeButton) {
+            closeButton.addEventListener('click', e => {
+                e.stopPropagation();
+                toast.hide();
+            });
+        }
     });
 });
