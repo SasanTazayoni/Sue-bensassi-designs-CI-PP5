d24e38917087ab3d419a8d9440899e9467c6a790
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-05-30 17:16:44 +0000

    Update view to prevent user from adding excess product quantity to cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 
 def view_cart(request):
@@ -10,14 +13,19 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart. """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
-        cart[item_id] += quantity
-    else:
-        cart[item_id] = quantity
+        if product.stock >= cart[item_id] + quantity:
+            cart[item_id] += quantity
+        else:
+            messages.error(
+                request, f'Error {product.name} has only {product.stock} units \
+                    left, you currently have {cart[item_id]} in your cart.'
+            )
 
     request.session['cart'] = cart
     return redirect(redirect_url)
