4ace34a8eb500dab65a5f845bb0c4e3ea5da1680
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-06-11 14:03:14 +0000

    Collect static files for Heroku

--- staticfiles/js/navbar.js
+++ staticfiles/js/navbar.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = scrollFunction;
     
 function scrollFunction() {
     const navbar = document.querySelector('.navbar');
@@ -8,25 +8,3 @@ function scrollFunction() {
         navbar.classList.remove("scrolled");
     }
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Function to update margin-top of the top container
-    function updateMarginTop() {
-        // Get the height of the navbar
-        const navbarHeight = document.querySelector('header').offsetHeight + 5; // Add 5 pixels
-
-        // Get the container element with the data attribute
-        const topContainer = document.querySelector('[data-top-block]');
-        
-        // If container exists, update its margin-top
-        if (topContainer) {
-            topContainer.style.marginTop = navbarHeight + 'px';
-        }
-    }
-
-    // Initial update when the page is loaded
-    updateMarginTop();
-
-    // Update when window is resized
-    window.addEventListener('resize', updateMarginTop);
-});
