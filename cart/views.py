0fa8f1cf897cf706b297ba0e25f402d8d55e0906
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-06-12 21:57:46 +0000

    Correct the redirect path when adding an item to the cart from the products template

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.contrib import messages
 from products.models import Product
+from urllib.parse import urlencode
 
 
 def view_cart(request):
@@ -14,23 +15,29 @@ def add_to_cart(request, item_id):
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+    redirect_url = request.POST.get('redirect_url') or reverse('products')
+
     cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
+    if item_id in cart:
         if product.stock >= cart[item_id] + quantity:
             cart[item_id] += quantity
             messages.success(request, f'{quantity} {product.name} added to your cart.')
         else:
             messages.error(
-                request, f'Error {product.name} has only {product.stock} units \
-                    left, you currently have {cart[item_id]} in your cart.'
+                request, f'Error: {product.name} has only {product.stock} units left. \
+                    You currently have {cart[item_id]} in your cart.'
             )
     else:
         cart[item_id] = quantity
         messages.success(request, f'{quantity} {product.name} added to your cart.')
 
     request.session['cart'] = cart
+
+    # Construct the redirect URL with preserved query parameters
+    if request.GET:
+        redirect_url += '?' + urlencode(request.GET)
+
     return redirect(redirect_url)
 
 
