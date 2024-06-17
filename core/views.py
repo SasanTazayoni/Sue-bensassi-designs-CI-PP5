9836bec8f845bc589a38c2fd4eff938606447968
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-06-17 21:56:20 +0000

    Create delivery template, url and view

--- core/views.py
+++ core/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     """ Renders the home page. """
-    
+
     return render(request, 'core/index.html')
 
 
 def about(request):
     """ Renders the about page. """
-    
+
     return render(request, 'core/about.html')
+
+def delivery(request):
+    """ Renders the delivery page. """
+
+    return render(request, 'core/delivery.html')
