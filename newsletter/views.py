488208b2512cb7dd9677f2cf481335ca75f511ac
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-06-18 14:19:06 +0000

    Add urls and view for newsletter subscriptions

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .models import Newsletter
+from .forms import NewsletterForm
 
-# Create your views here.
+
+def newsletter_subscribe(request):
+    """ 
+    A view to handle new sign ups to the newsletter mailing list.
+    """
+
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(
+                request, 'You have subscribed to our mailing list.'
+            )
+            return redirect(reverse('home'))
+        else:
+            messages.error(
+                request, 'Failed to sign up. Please ensure the form is valid.'
+            )
+    else:
+        form = NewsletterForm()
+
+    context = {
+        'newsletter_form': form,
+    }
+
+    return redirect(reverse('home'))
+
--- sue_bensassi_designs/urls.py
+++ sue_bensassi_designs/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('newsletter/', include('newsletter.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'sue_bensassi_designs.views.handler404'
