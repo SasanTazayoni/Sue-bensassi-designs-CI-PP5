eae8b344338f72471303f2ea2f671116e9a63817
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-06-18 19:45:58 +0000

    Adjust newsletter subscribe view to send a welcome email to a new user

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import (
+    render,
+    redirect,
+    reverse,
+    get_object_or_404
+)
 from django.contrib import messages
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from .models import NewsletterSubscription
 from .forms import NewsletterForm
 
@@ -12,10 +20,26 @@ def newsletter_subscribe(request):
     if request.method == 'POST':
         form = NewsletterForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_subscription = form.save()
             messages.success(
                 request, 'You have subscribed to our mailing list.'
             )
+
+            user_email = new_subscription.email
+            subject = render_to_string(
+                'newsletter/new_subscription_subject.txt')
+            body = render_to_string(
+                'newsletter/new_subscription_body.txt',
+                {'contact_email': settings.DEFAULT_FROM_EMAIL}
+            )
+
+            send_mail(
+                subject,
+                body,
+                settings.DEFAULT_FROM_EMAIL,
+                [user_email]
+            )
+
             return redirect(reverse('home'))
 
         else:
