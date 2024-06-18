0c63bc32f50bc8fc1c258b9eb01ae66f025cdc15
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-06-18 13:36:22 +0000

    Show new newsletter sign ups in admin panel

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Newsletter
 
-# Register your models here.
+
+class NewsletterAdmin(admin.ModelAdmin):
+    ordering = ('email',)
+
+
+admin.site.register(Newsletter, NewsletterAdmin)
