31e58f6a5f62ecfa86478d2b6b9bc07971193a82
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-06-19 13:02:32 +0000

    Add Enquiry model to admin panel with ordering by date

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Enquiry
 
-# Register your models here.
+@admin.register(Enquiry)
+class EnquiryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'date_sent', 'replied_to')
+    list_filter = ('replied_to', 'date_sent')
+    search_fields = ('name', 'email', 'message')
+    ordering = ['date_sent']
