0ceff5a45d2c246d8c178acacee3104587102a88
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-06-19 14:10:45 +0000

    Register fake items in admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, FakeItem
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -22,5 +22,10 @@ class CategoryAdmin(admin.ModelAdmin):
     )
 
 
+class FakeItemAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(FakeItem, FakeItemAdmin)
