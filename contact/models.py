b6a09c04fc75f88ea8f43221df95ed5ddd05ac7b
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-06-19 13:01:14 +0000

    Create model for contact enquiries and migrate

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Enquiry(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+    message = models.TextField(max_length=1000)
+    date_sent = models.DateTimeField(auto_now_add=True)
+    replied_to = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['date_sent']
+
+    def __str__(self):
+        return f"Enquiry from {self.name} ({self.email})"
