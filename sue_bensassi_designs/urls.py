bd738ce7bd85f47e6375b49607085cf0e94f69d4
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-06-19 12:09:14 +0000

    Add contact to project level urls

--- sue_bensassi_designs/urls.py
+++ sue_bensassi_designs/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('core.urls')),
+    path('contact/', include('contact.urls')),
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
