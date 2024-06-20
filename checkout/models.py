de7e4cc2a1e98e6115530a2701f1c852b486e1e2
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-06-20 16:34:23 +0000

    Update checkout models for Python linter

--- checkout/models.py
+++ checkout/models.py
@@ -8,9 +8,9 @@ from profiles.models import UserProfile
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     user_profile = models.ForeignKey(
-                        UserProfile, on_delete=models.SET_NULL,
-                        null=True, blank=True, related_name='orders'
-                        )
+        UserProfile, on_delete=models.SET_NULL,
+        null=True, blank=True, related_name='orders'
+    )
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=100, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -21,14 +21,12 @@ class Order(models.Model):
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     grand_total = models.DecimalField(
-                        max_digits=10, decimal_places=2,
-                        null=False, default=0
-                        )
+        max_digits=10, decimal_places=2, null=False, default=0
+    )
     original_cart = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(
-                        max_length=254, null=False,
-                        blank=False, default=''
-                        )
+        max_length=254, null=False, blank=False, default=''
+    )
 
     def _generate_order_number(self):
         """ Generate a random, unique order number using UUID. """
