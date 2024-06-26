dbb6b75ffbfed070555dbd72600377208644c930
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-06-26 18:56:34 +0000

    Add python unit testing for core views

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestHomeView(TestCase):
+
+    def test_home_view(self):
+        """
+        Test the home view.
+        """
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/index.html')
+
+
+class TestAboutView(TestCase):
+
+    def test_about_view(self):
+        """
+        Test the about view.
+        """
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/about.html')
+
+
+class TestDeliveryView(TestCase):
+
+    def test_delivery_view(self):
+        """
+        Test the delivery view.
+        """
+        response = self.client.get(reverse('delivery'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/delivery.html')
+
+
+class TestTermsView(TestCase):
+
+    def test_terms_view(self):
+        """
+        Test the terms & conditions view view.
+        """
+        response = self.client.get(reverse('terms'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/terms.html')
