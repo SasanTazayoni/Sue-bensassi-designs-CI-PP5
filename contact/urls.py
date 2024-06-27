c36cdb826b0e5d7cc7d2cdc2aea654b2c3c348a2
Sasan Tazayoni <sasantazayoni@gmail.com>
2024-06-27 10:23:07 +0000

    Update urls.py for contact app

--- contact/urls.py
+++ contact/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.contact, name='contact'),
     path('success/', views.contact_success, name='contact_success'),
+    path('edit/<int:enquiry_id>/',
+         views.edit_enquiry, name='edit_enquiry'),
+    path('delete/<int:enquiry_id>/',
+         views.delete_enquiry, name='delete_enquiry'),
 ]
